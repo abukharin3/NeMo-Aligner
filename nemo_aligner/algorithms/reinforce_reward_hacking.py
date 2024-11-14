003ac6aa924b6fe8f173db6ff8c6974d81b3f138
abukharin <abukharin@nvidia.com>
2024-11-13 16:43:47 -0800

    Change reward formulation

--- nemo_aligner/algorithms/reinforce_reward_hacking.py
+++ nemo_aligner/algorithms/reinforce_reward_hacking.py
@@ -331,10 +331,7 @@ class ReinforceHacker:
                 future_min = futures_min[i]
                 rewards_max = future_max.result()
                 rewards_min = future_min.result()
-                if self.reward_max is None:
-                    rewards = self.cfg.lam1 * rewards_max - self.cfg.lam2 * rewards_min
-                else:
-                    rewards = self.cfg.lam1 * rewards_max - self.cfg.lam2 * rewards_min / (torch.clip(self.cfg.reward_anchor - rewards_max.mean(), min=0) ** self.cfg.gamma_reward + 1)
+                rewards = self.cfg.lam1 * rewards_max - self.cfg.lam2 * rewards_min / (torch.clip(self.cfg.reward_anchor - rewards_max.mean(), min=0) ** self.cfg.gamma_reward + 1)
                 rm_value_rollout_batches.append({"rewards": self.cfg.lam1 * rewards_max - self.cfg.lam2 * rewards_min, "rewards_to_max":rewards_max, "rewards_to_min": rewards_min})
             timer_metrics["critic_wait"] = self.timer.stop_and_get_time("critic_wait")
 
