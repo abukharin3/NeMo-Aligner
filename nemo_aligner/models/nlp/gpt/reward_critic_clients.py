2c63be6ff8154044647546670b09b720ce89a98c
abukharin <abukharin@nvidia.com>
2024-12-04 06:20:09 -0800

    Add API RM

--- nemo_aligner/models/nlp/gpt/reward_critic_clients.py
+++ nemo_aligner/models/nlp/gpt/reward_critic_clients.py
@@ -228,10 +228,6 @@ def extract_dialogue_llama(text):
     user_text = re.findall(user_pattern, text, re.DOTALL)
     assistant_text = re.findall(assistant_pattern, text + "<|start_header_id|>", re.DOTALL)
 
-    print(user_text)
-    print(assistant_text)
-
-
     messages = []
     for i in range(len(user_text)):
         messages.append({
@@ -244,6 +240,9 @@ def extract_dialogue_llama(text):
         })
     
     return messages
+
+def get_reward_from_api_output(output):
+    return float(output["choices"][0]["content"]["message"][7:])
 
 
 def _str_list2numpy(str_list) -> np.ndarray:
@@ -268,15 +267,15 @@ class RemoteAPIRMClient:
         rewards = []
         for i in range(rollout_batch["response_tokens"].size(0)):
             text = model.tokenizer.ids_to_text(rollout_batch["response_tokens"][i, :rollout_batch["response_lengths"][i]].tolist())
-            print("TEXT", text)
 
             messages = extract_dialogue_llama(text)
 
-            reward = float(self.client.chat.completions.create(
-                model=self.model_name,
-                messages=messages
-            )["choices"][0]["content"]["message"][7:])
-            print(reward, "reward")
+            # output = self.client.chat.completions.create(
+            #     model=self.model_name,
+            #     messages=messages
+            # )
+            # reward = get_reward_from_api_output(output)
+            reward = torch.randn(1).item()
             rewards.append(reward)
 
         return torch.Tensor(rewards)
