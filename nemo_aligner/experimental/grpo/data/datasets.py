46888f0d3475f9ad2784bfb7bc7b196442b09028
Alexander Bukharin <abukharin3@gatech.edu>
2025-02-15 10:00:53 -0500

    Add flexible sys prompt

--- nemo_aligner/experimental/grpo/data/datasets.py
+++ nemo_aligner/experimental/grpo/data/datasets.py
@@ -77,6 +77,7 @@ class AllTaskDataset:
         else:
             text = self.prompt.format(text_str)
 
+        print("system_prompt", system_prompt)
         print("text", text)
         sample, _ = self.encode(text)
         sample_tensor = torch.as_tensor(sample, dtype=torch.int64)
