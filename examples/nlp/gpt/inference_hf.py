b9b0d198bc83e8913e6c74defd3d4edf034739a8
Alexander Bukharin <abukharin3@gatech.edu>
2025-02-17 15:57:20 -0500

    try hf inference

--- examples/nlp/gpt/inference_hf.py
+++ examples/nlp/gpt/inference_hf.py
@@ -163,7 +163,7 @@ def main(cfg) -> None:
     rm_critic_min = RemoteGPTRMClient(cfg.remote_critic_rm_min)
     
 
-    time.sleep(300) # Allow hf server to start
+    time.sleep(800) # Allow hf server to start
 
     timer = Timer(cfg.exp_manager.get("max_time_per_run"))
 
