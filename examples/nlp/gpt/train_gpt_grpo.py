1bd1883c999b7dbfa482e9c0cbe380b255a43edb
Alexander Bukharin <abukharin3@gatech.edu>
2025-03-25 16:37:05 -0400

    Add ifeval

--- examples/nlp/gpt/train_gpt_grpo.py
+++ examples/nlp/gpt/train_gpt_grpo.py
@@ -31,6 +31,7 @@ from nemo_aligner.experimental.grpo.data.datasets import AllTaskDataset
 from nemo_aligner.experimental.grpo.models.nlp.gpt.megatron_gpt_grpo_actor import MegatronGPTActorModel
 from nemo_aligner.experimental.grpo.experience.environments.math_environment import MathEnvironment
 from nemo_aligner.experimental.grpo.experience.environments.code_environment import CodeEnvironment
+from nemo_aligner.experimental.grpo.experience.environments.ifeval_environment import IFEvalEnvironment
 from nemo_aligner.experimental.grpo.experience.rollout_generator import SequenceRewardRolloutGenerator
 from nemo_aligner.utils import parallel_state
 from nemo_aligner.utils.batch_iterators import get_batch_iterator_cls
@@ -170,6 +171,7 @@ def main(cfg) -> None:
     tasks_to_environments = {k:MathEnvironment(cfg.trainer.grpo.environments.math) for k in {"aime24", "amc23", "math", "qwq_sol_gen_no_ans_c4", "qwq_sol_gen_no_ans_c7", "qwq_sol_gen_no_ans_olymp_pr_gt03", "qwq_sol_gen_no_ans_olymp_pr_lt03", "deepscaler"}}
     tasks_to_environments["code"] = CodeEnvironment(cfg.trainer.grpo.environments.code)
     tasks_to_environments["code_mbppplus_test"] = CodeEnvironment(cfg.trainer.grpo.environments.code)
+    tasks_to_environments["ifeval"] = IFEvalEnvironment(cfg.trainer.grpo.environments.ifeval)
     rollout_generator = SequenceRewardRolloutGenerator(cfg.trainer.grpo, tasks_to_environments)
 
     timer = Timer(cfg.exp_manager.get("max_time_per_run") if cfg.exp_manager else None)
--- nemo_aligner/experimental/grpo/data/datasets.py
+++ nemo_aligner/experimental/grpo/data/datasets.py
@@ -76,6 +76,9 @@ class AllTaskDataset:
         if task_name == "code":
             text_str = self.data[idx]["prompt"]
             extra_verifier_info = {"unittests": self.data[idx]["args"]["unittests"], "test_type": self.data[idx]["args"]["test_type"], "fn_name": self.data[idx]["args"].get("fn_name", None)}
+        elif task_name == "ifeval":
+            text_str = self.data[idx]["prompt"]
+            extra_verifier_info = {"args": self.data[idx]["args"]}
         elif ("args" in self.data[idx] and task_name == "deepscaler") or "text" in self.data[idx]:
             text_str = self.data[idx]["text"]
             extra_verifier_info = {"ground_truth": self.data[idx]["args"]["answer"]}
