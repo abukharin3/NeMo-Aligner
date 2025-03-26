73a780e269a249f6dced7af6ea7e090499f509f5
Alexander Bukharin <abukharin3@gatech.edu>
2025-03-26 13:06:54 -0400

    debug response length

--- nemo_aligner/experimental/grpo/algorithms/grpo.py
+++ nemo_aligner/experimental/grpo/algorithms/grpo.py
@@ -141,7 +141,7 @@ class GRPOTrainer:
         rewards = rollout_batch["rewards"]
 
         if self.cfg.length_penalty_enabled:
-            rewards = -1 * (1 - rewards) + -1 *rewards * (rollout_batch["response_lengths"] / self.cfg.max_sequence_length)
+            rewards = -1 * (1 - rewards) + -1 *rewards * ((rollout_batch["response_lengths"] - rollout_batch["prompt_lengths"]) / self.cfg.max_sequence_length)
 
         baseline, reward_std = calculate_baseline_and_std_per_prompt(
                 prompts=rollout_batch["text"],
